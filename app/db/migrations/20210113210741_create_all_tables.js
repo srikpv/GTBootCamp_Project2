374a0ddd4fafe6aa135103dad2eec9fe96ab2457
srikpv <pillarisetty.s@gmail.com>
2021-01-14 11:54:12 -0500

    Knex changes, DB Schema

--- app/db/migrations/20210113210741_create_all_tables.js
+++ app/db/migrations/20210113210741_create_all_tables.js
@@ -1,24 +1,42 @@
 'use strict';
 
 exports.up = async (knex) => {
-  await knex.schema.createTable('player', (table) => {
-    table.increments().primary();
+  return knex.schema.createTable('player', (table) => {
+    table.increments("id").primary();
     table.string('name').notNullable();
     table.string('country').notNullable();
     table.string('dob').notNullable();
+  })
+  .createTable("user", (table) => {
+    table.increments("id").primary();
+    table.string("username").notNullable();
+    table.string("password").notNullable();
+  })
+  .createTable("team", (table) => {
+    table.increments("id").primary();
+    table.integer("user_id").unsigned().references("id").inTable("user").onDelete('SET NULL').index();
+    table.string("name").notNullable();
+    table.boolean("active").notNullable().defaultTo(true);
+    table.integer("wins").notNullable().defaultTo(0);
+  })
+  .createTable("team_player", (table) => {
+    table.increments("id").primary();
+    table.integer("team_id").unsigned().references("id").inTable("team").onDelete('SET NULL').index();
+    table.integer("player_id").unsigned().references("id").inTable("player").onDelete('SET NULL').index();
+    table.integer("score").notNullable().defaultTo(0);
+  })
+  .createTable("game", (table) => {
+    table.increments("id").primary();
+    table.integer("home_team_id").unsigned().references("id").inTable("team").onDelete('SET NULL').index();
+    table.integer("opp_team_id").unsigned().references("id").inTable("team").onDelete('SET NULL').index();
+    table.integer("win_team_id").unsigned().references("id").inTable("team").onDelete('SET NULL').index();
   });
-
-  await knex.schema.createTable('person', (table) => {
-    table.increments('id').primary();
-    table.string('firstName');
-    table.string('lastName');
-  });
-
-  return true;
 };
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('player');
-  await knex.schema.dropTableIfExists('person');
-  return true;
+  return knex.schema.dropTableIfExists('game')
+  .dropTableIfExists('team_player')
+  .dropTableIfExists('team')
+  .dropTableIfExists('user')
+  .dropTableIfExists('player');
 };
--- app/db/seeds/players.js
+++ app/db/seeds/players.js
@@ -5,27 +5,33 @@ const path = require("path");
 let players = [];
 let players_subset = [];
 
-fs.createReadStream(path.resolve(__dirname, 'Players.csv'))
+var fileRead = _ => {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(path.resolve(__dirname, './Players.csv') )
     .pipe(csv())
     .on('data', (data) => players.push(data))
-    .on('end', async () => {
+    .on('end', () => {
         players = players.filter(player => new Date(player.dob) > new Date("01/01/1990"));
-        players.forEach((player) => {
-            players_subset.push({name: player.name, country: player.country, dob: player.dob});
+        players.forEach((player, index) => {
+          players_subset.push({id: index+1, name: player.name, country: player.country, dob: player.dob});
         });
-    });
+       return resolve(players_subset);
+    })
+    .on("error", (err) => reject(err));
+  });
+}
 
-exports.seed = async (knex, Promise) => {
-    // Deletes ALL existing entries
-    
-    await sleep(3000);
-    await knex('player').del();       
-    await knex('player').insert(players_subset);
-    return true;
-  };
-
-  function sleep(ms) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, ms);
+exports.seed = (knex) => {
+  // Deletes ALL existing entries
+  var promise = fileRead();
+  return promise
+  .then((data) => {
+    return knex('player').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('player').insert(data);
     });
-  }   
+  });
+};
+
+ 
