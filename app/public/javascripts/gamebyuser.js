30bd8bfb3d2783e8d6ffbc113ef470c2fa897506
Rakesh <arorark77@gmail.com>
2021-01-19 19:29:18 -0500

    game history  by user and team

---
+++ app/public/javascripts/gamebyuser.js
@@ -0,0 +1,85 @@
+$(document).ready(function() {
+  // var userList = document.getElementById("user-list");
+  var gameList = $("tbody");
+  var gameContainer = $(".history-container");
+  getUser();
+
+  $("#user-list").change(function(){
+    var $userdropdown = $(this);
+    var userId = $userdropdown.val();
+    console.log(userId);
+    getuserTeams(userId);
+  });
+
+  $("#team-list").change(function(){
+    var $teamdropdown = $(this);
+    var teamId = $teamdropdown.val();
+    console.log(teamId);
+    getuserGames(teamId);
+  });
+ 
+      // Function for retrieving authors and getting them ready to be rendered to the page
+      function getUser() {
+        $.get("http://localhost:3000/api/all/users", function(data) {
+        console.log(data);
+        var $option = $("#user-list");      
+          for (var i = 0; i < data.users.length; i++) {
+            $option.append($("<option></option>").attr("value",data.users[i].id).text(data.users[i].username));          
+          }
+          // console.log(userList)
+        });
+      }
+       
+      function getuserTeams() {
+        $.get("http://localhost:3000/api/all/teams", function(data) {
+          // data = data.filter(team => team.user_id = parseInt($userdropdown.val()));
+        console.log(data);
+        var $option = $("#team-list");      
+        for (var i = 0; i < data.teams.length; i++) {
+          $option.append($("<option></option>").attr("value",data.teams[i].id).text(data.teams[i].name));          
+        }
+        })
+      }
+
+      function getuserGames() {
+        $.get("http://localhost:3000/api/all/games", function(data) {
+          console.log(data);
+          var rowsToAdd = [];
+          for (var i = 0; i < data.games.length; i++) {
+            rowsToAdd.push(createGameRow(data.games[i]));        
+          }
+          renderGameList(rowsToAdd);
+          
+        })
+      }
+
+      function createGameRow(gameData) {
+        var newTr = $("<tr>");
+        newTr.data("game", gameData);
+        newTr.append("<td>" + gameData.home_team_id + "</td>");
+        newTr.append("<td>" + gameData.opp_team_id + "</td>");
+        newTr.append("<td>" + gameData.win_team_id + "</td>");
+        return newTr;    
+      }
+
+      function renderGameList(rows) {
+        gameList.children().not(":last").remove();
+        gameContainer.children(".alert").remove();
+        if (rows.length) {
+          gameList.prepend(rows);
+        }
+        else {
+          renderEmpty();
+        }
+      }
+
+      function renderEmpty() {
+        var alertDiv = $("<div>");
+        alertDiv.addClass("alert alert-danger");
+        alertDiv.text("No Games Played Yet, Check in Later !!!");
+        gameContainer.append(alertDiv);
+      }    
+});
+
+
+  
